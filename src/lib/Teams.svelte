11b0804b45c47ea87f9d7164136495ff947499cf
Manoel Castro <manoelcastrodev@gmail.com>
2022-09-24 12:19:56 -0300

    chore: separador de lista

--- src/lib/Teams.svelte
+++ src/lib/Teams.svelte
@@ -7,15 +7,40 @@
   <div class="h-full w-96 p-2 bg-slate-50 text-blue-600 border-blue-400 rounded-lg border-8 overflow-y-scroll scrollbar-thin scrollbar-thumb-blue-100 scrollbar-thumb-rounded">
     {#each people as person}
       {#if person.vote === 'boy'}
-        <li class="list-none">{person.name}</li>
+        <li class="lista-boy list-none leading-9 font-light">{person.name}</li>
       {/if}
     {/each}
   </div>
   <div class="h-full w-96 p-2 bg-slate-50 text-pink-600 border-pink-400 rounded-lg border-8 overflow-y-scroll scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-pink-100">
     {#each people as person}
       {#if person.vote === 'girl'}
-        <li class="list-none">{person.name}</li>
+        <li class="lista-girl list-none leading-9 font-light">{person.name}</li>
       {/if}
     {/each}
   </div>
 </div>
+
+<style lang="scss">
+  .lista-boy, .lista-girl {
+    position: relative;
+  }
+
+  .lista-boy::after, .lista-girl::after {
+    content: '';
+    position: absolute;
+    height: 0.1em;
+    width: 5%;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+  }
+
+  .lista-girl::after {
+    background-color: #fce7f3;
+  }
+
+  .lista-boy::after {
+    background-color: #dbeafe;
+  }
+</style>
