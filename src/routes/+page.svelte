27be26fe14407a1cdc2225adee44efab2b6e1c36
Manoel Castro <manoelcastrodev@gmail.com>
2022-09-24 04:16:17 -0300

    feat: - Iniciando componente de times de votacao
    chore:
    - Organizando componentes Vote e Teams na pasta lib.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,20 @@
 <script lang="ts">
   import Background from "$lib/Background.svelte";
-  let metrica: number = 50;
+  import Vote from '$lib/Vote.svelte'
+  import Teams from '$lib/Teams.svelte'
+
+  let metrica: number;
 
   $: boy = metrica
   $: girl = 100 - metrica
-
-  function handleClickBoy() {
-    metrica += 1
-  }
-
-  function handleClickGirl() {
-    metrica -= 1
-  }
 
 </script>
 
 <Background rank={{boy, girl}}>
   <div class="min-h-screen min-w-full flex justify-center items-center">
-    <div class="h-24 w-96 p-8 bg-slate-50 rounded-md flex flex-col justify-center">
-      <input class="" type="range" min="0" max="100" bind:value={metrica} hidden/>
-      <div class="flex justify-between">
-        <button on:click={() => handleClickBoy()}>menino</button>
-        <button on:click={() => handleClickGirl() }>menina</button>
-      </div>
+    <div class="flex flex-col justify-between items-center gap-8">
+      <Vote bind:metrica />
+      <Teams />
     </div>
   </div>
 </Background>
