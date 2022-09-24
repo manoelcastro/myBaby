57163e714f89895e00af93bace754629c1ca1ef7
Manoel Castro <manoelcastrodev@gmail.com>
2022-09-24 04:28:43 -0300

    chore: Iniciando a dinamica dos teams

--- src/lib/Teams.svelte
+++ src/lib/Teams.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+  export let people;
+</script>
+
 <div class="flex justify-between gap-28 min-h-max">
   <div class="h-24 w-96 bg-slate-50 rounded-lg border-8 border-blue-400"></div>
   <div class="h-24 w-96 bg-slate-50 rounded-lg border-8 border-pink-400"></div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,28 @@
   import Vote from '$lib/Vote.svelte'
   import Teams from '$lib/Teams.svelte'
 
+  interface IPerson {
+      name: string;
+      avatar: string;
+      vote: string;
+      pix: boolean;
+  }
+
+  const people: IPerson[] = [
+    {
+      name: 'Joaquim Pereira',
+      avatar: '/',
+      vote: 'boy',
+      pix: false
+    },
+    {
+      name: 'Manoel Castro',
+      avatar: '/',
+      vote: 'girl',
+      pix: true
+  }
+]
+
   let metrica: number;
 
   $: boy = metrica
@@ -14,7 +36,7 @@
   <div class="min-h-screen min-w-full flex justify-center items-center">
     <div class="flex flex-col justify-between items-center gap-8">
       <Vote bind:metrica />
-      <Teams />
+      <Teams {people} />
     </div>
   </div>
 </Background>
